77e9da05423208cc74b3774ac9f8093ec4a3dcf7
Laning74 <91938980+Laning74@users.noreply.github.com>
2023-04-12 10:48:52 +0200

    triangle images and install ngrok

--- App.tsx
+++ App.tsx
@@ -1,18 +1,15 @@
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import "react-native-gesture-handler";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import DrawerNavigator from "./components/DrawerNavigator";
 
-import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import 'react-native-gesture-handler';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import DrawerNavigator from './components/DrawerNavigator';
-
-
-
-const Stack = createNativeStackNavigator()
-
+const Stack = createNativeStackNavigator();
 
 export default function App(): JSX.Element {
-
-{/* isAuth comes here */}
+  {
+    /* isAuth comes here */
+  }
   return (
     <NavigationContainer>
       <DrawerNavigator />
