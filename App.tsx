96fe0c4d0a092f7ceed5591668ac3998af2a8ebe
Laning74 <91938980+Laning74@users.noreply.github.com>
2023-04-27 09:25:28 +0200

    No header to Login page

--- App.tsx
+++ App.tsx
@@ -4,18 +4,12 @@ import "react-native-gesture-handler";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import DrawerNavigator from "./components/DrawerNavigator";
 
-
-const Stack = createNativeStackNavigator()
+const Stack = createNativeStackNavigator();
 
 export default function App(): JSX.Element {
-
- 
-
   return (
-    
     <NavigationContainer>
       <DrawerNavigator />
     </NavigationContainer>
-  
   );
-} 
+}
