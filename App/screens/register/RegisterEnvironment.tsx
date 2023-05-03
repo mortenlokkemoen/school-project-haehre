7e66e75e11bf5138b0bc6dd2ae21fb3f0afa7ef3
Harpreetd <dhimanharpreet17@yahoo.in>
2023-05-03 13:04:33 +0200

    ytremiljø

--- App/screens/register/RegisterEnvironment.tsx
+++ App/screens/register/RegisterEnvironment.tsx
@@ -1,97 +1,85 @@
-import { View, StyleSheet, Text, TextInput } from "react-native";
+import { View, StyleSheet, Text, TextInput, ScrollView } from "react-native";
 import { IStackScreenProps } from "../../../src/library/StackScreenProps";
 import RadioButtons from "../../../components/RadioButtons";
 import DatetimePicker from "../../../components/DatetimePicker";
+
+import { TriangleDown } from "../../../components/TriangleDown";
+import RadioButtonImage from "../../../components/RadioButtonImage";
+import { useEffect, useState } from "react";
 import Title from "../../../components/Title";
 
 const RegisterEnvironmentScreen: React.FunctionComponent<IStackScreenProps> = (
   props
 ) => {
   const { navigation, route, nameProp } = props;
-  console.log({ navigation, route, nameProp });
+  const [descriptionText, setDescriptionText] = useState("");
 
+  useEffect(() => {
+    navigation.addListener("focus", () => {
+      setDescriptionText("");
+    });
+  });
+
+  const handleTextInput = (text: string) => {
+    setDescriptionText(text);
+  };
   return (
-    <View style={styles.inputContainer}>
-      <View style={styles.buttonsContainer}>
-        <View style={styles.buttonContainer}>
-          <Title>Dato kommer her</Title>
-          <Text style={styles.paragraph}>YTRE MILJØ</Text>
-          <View style={styles.textinputContainer}>
-            <TextInput>Beskriv hendelse</TextInput>
-          </View>
-          <View style={styles.dateContainer}>
-            <DatetimePicker />
-          </View>
-          <View style={styles.paragraphContainer}>
-            <Text style={styles.paragraph}>
-              Er det gjennomført strakstiltak?
-            </Text>
-          </View>
-        </View>
-      </View>
-      <View style={styles.radioButtonsContainer}>
+    <ScrollView style={styles.container}>
+      <TriangleDown />
+      <View style={styles.maincontainer}>
+        <Title>Ytremiljø</Title>
+        <TextInput
+          style={styles.textinputContainer}
+          placeholder="Beskrive Hendelse"
+          multiline
+          numberOfLines={4}
+          onChangeText={handleTextInput}
+          value={descriptionText}
+        />
+        <DatetimePicker />
+        <View
+          style={{
+            borderBottomColor: "black",
+            borderBottomWidth: StyleSheet.hairlineWidth,
+          }}
+        />
+        <Text style={styles.paragraph}>Er det gjennomført strakstiltak?</Text>
         <RadioButtons />
+        <Text style={styles.paragraph}>Vil du ta bilde?</Text>
+        <RadioButtonImage
+          navigation={navigation}
+          route={route}
+          nameProp={nameProp}
+        />
       </View>
-    </View>
+    </ScrollView>
   );
 };
 
 export default RegisterEnvironmentScreen;
 
 const styles = StyleSheet.create({
-  inputContainer: {
-    // justifyContent: "center",
-    // alignItems: "center",
-    // marginTop: 100,
-    // marginHorizontal: 24,
-    // padding: 16,
+  container: {
+    flex: 1,
+    backgroundColor: "#ecedee",
   },
-  textContainer: {
+  maincontainer: {
     justifyContent: "center",
     alignItems: "center",
-    marginTop: 40,
-    marginHorizontal: 24,
-    padding: 16,
+    backgroundColor: "#ecedee",
+    height: "auto",
   },
-
   paragraph: {
     fontSize: 18,
     marginTop: 15,
     marginBottom: 25,
   },
-  paragraphContainer: {
-    width: "140%",
-    justifyContent: "center",
-    alignItems: "center",
-    borderBottomColor: "black",
-    borderBottomWidth: 1,
-  },
   textinputContainer: {
     backgroundColor: "white",
-    width: "100%",
+    width: "80%",
     height: 100,
-    padding: 15,
-  },
-
-  dateContainer: {
-    backgroundColor: "white",
-    width: "100%",
-    height: 100,
-    padding: 15,
-  },
-
-  radioButtonsContainer: {
-    flexDirection: "row",
-    justifyContent: "space-around",
-    alignItems: "center",
-    // marginTop: 10,
-    // marginBottom: 25,
-  },
-
-  buttonsContainer: {
-    // flexDirection: "row",
-  },
-  buttonContainer: {
-    // flex: 1,
+    marginBottom: 15,
+    padding: 10,
+    textAlignVertical: "top",
   },
 });
