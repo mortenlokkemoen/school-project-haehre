48c632dc497799e1c841785ae06c8898c6f2394e
Laning74 <91938980+Laning74@users.noreply.github.com>
2023-05-05 19:32:54 +0200

    testin login

--- components/DrawerNavigator.tsx
+++ components/DrawerNavigator.tsx
@@ -1,98 +1,19 @@
 import React from "react";
-import { View, Image, StyleSheet } from "react-native";
-import {
-  DrawerContent,
-  createDrawerNavigator,
-  DrawerToggleButton,
-} from "@react-navigation/drawer";
-import routes from "../src/config/routes";
-import { useFonts, Barlow_600SemiBold } from "@expo-google-fonts/barlow";
-import * as SplashScreen from "expo-splash-screen";
-import { useCallback } from "react";
-import BackButton from "../components/BackButton";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import NavigationOptions from "./navigationOption";
+import Login from "/";
 
-// Async font to load in before app
 const Drawer = createDrawerNavigator();
-SplashScreen.preventAutoHideAsync();
 
-const DrawerNavigator = () => {
-  const excludedRoutes = ["Checklist"];
-
-  const [fontsLoaded] = useFonts({
-    Barlow_600SemiBold,
-  });
-
-  const onLayoutRootView = useCallback(async () => {
-    if (fontsLoaded) {
-      await SplashScreen.hideAsync();
-    }
-  }, [fontsLoaded]);
-
-  if (!fontsLoaded) {
-    return null;
-  }
-
-  const filteredRoutes = routes.filter(
-    (route) => !excludedRoutes.includes(route.name)
-  );
-
+const DrawerNavigation = () => {
   return (
     <Drawer.Navigator
-      initialRouteName={"Login"}
-      drawerContent={(props) => <DrawerContent {...props} />}
-      screenOptions={({ route }) => ({
-        headerShown: route.name !== "Login",
-        headerStyle: { backgroundColor: "#003D6A", height: 120 },
-        headerTintColor: "#003d6a",
-        drawerContentContainerStyle: { backgroundColor: "#DCE0E6" },
-        drawerPosition: "right",
-        headerLeft: () => <BackButton />,
-        headerRight: () => <DrawerToggleButton tintColor="white" />,
-        headerTitle: () => (
-          <View style={styles.logoContainer} onLayout={onLayoutRootView}>
-            <Image
-              source={require("../assets/hæhrelogo-hvit.png")}
-              style={styles.logo}
-            />
-          </View>
-        ),
-      })}
-      backBehavior="history"
+      initialRouteName="Login"
+      screenOptions={NavigationOptions}
     >
-      {filteredRoutes.map((r, i) => (
-        <Drawer.Screen key={i} name={r.name}>
-          {(props) => {
-            const route = {
-              ...props.route,
-              params: {
-                event: props.route?.params || undefined,
-              },
-            };
-            return (
-              <r.component
-                nameProp={r.name}
-                navigation={props.navigation}
-                route={route}
-              />
-            );
-          }}
-        </Drawer.Screen>
-      ))}
+      <Drawer.Screen name="Login" component={Login} />
     </Drawer.Navigator>
   );
 };
 
-export default DrawerNavigator;
-
-const styles = StyleSheet.create({
-  logoContainer: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  logo: {
-    width: 100,
-    height: 100,
-    resizeMode: "contain",
-  },
-});
+export default DrawerNavigation;
---
+++ components/NavigationOption.tsx
@@ -0,0 +1,42 @@
+import React, { useCallback } from "react";
+import { View, Image, StyleSheet } from "react-native";
+import { DrawerToggleButton } from "@react-navigation/drawer";
+import BackButton from "./BackButton";
+import * as SplashScreen from "expo-splash-screen";
+
+const onLayoutRootView = useCallback(async () => {
+  await SplashScreen.hideAsync();
+}, []);
+
+const NavigationOptions = {
+  headerShown: true,
+  headerStyle: { backgroundColor: "#003D6A", height: 120 },
+  headerTintColor: "#003d6a",
+  drawerContentContainerStyle: { backgroundColor: "#DCE0E6" },
+  drawerPosition: "right",
+  headerLeft: () => <BackButton />,
+  headerRight: () => <DrawerToggleButton tintColor="white" />,
+  headerTitle: () => (
+    <View style={styles.logoContainer} onLayout={onLayoutRootView}>
+      <Image
+        source={require("../assets/hæhrelogo-hvit.png")}
+        style={styles.logo}
+      />
+    </View>
+  ),
+};
+
+const styles = StyleSheet.create({
+  logoContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  logo: {
+    width: 100,
+    height: 100,
+    resizeMode: "contain",
+  },
+});
+
+export default NavigationOptions;
